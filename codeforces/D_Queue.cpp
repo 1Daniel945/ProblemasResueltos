fa8c6a5e5a7f09fb02284d998d50b2e00e00e616
1Daniel945 <xdaniel945x@gmail.com>
2025-09-22 23:14:09 -0600

    New solved problem Queue

---
+++ codeforces/D_Queue.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+#include <cstdlib>
+#include <ctime>
+#define fr(i, x, n) for(int i = x; i<(int)n; ++i)
+#define fi(i, x, n) for(int i = (int)x; i >= n; --i)
+#define in >>
+#define p <<
+#define fast_io ios :: sync_with_stdio(0); cin.tie(0);
+#define ff first
+#define ss second
+#define pb push_back
+#define ac accumulate
+#define lsb(x) ((x) & -(x)) //less significative bit
+#define out(i, n) (i == (int) n - 1 ? "\n" : " ")
+typedef long long ll;
+typedef unsigned long long ull;
+using namespace std;
+
+ull lim = (ull) 2e64;
+int MOD = 1e9 + 7;
+
+void solve(){
+    int n; cin in n;
+    vector<int>a(n);
+    fr(i, 0, n) cin in a[i];
+    sort(a.begin(), a.end());
+    ll waitTime = 0, x = 0;
+    fr(i, 0, n){
+        if(waitTime <= a[i]){
+            waitTime += a[i];
+            x++;
+        }
+    }
+    cout p x p "\n";
+}
+
+int main(){
+    fast_io
+    solve();
+    return 0;
+} 
